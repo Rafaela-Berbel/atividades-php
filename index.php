83e99bcfb8fbed647164f91c5fca8be3ce4005a5
Rafaela Ferraz Daher Berbel <ra2557100@MacBook-Air-de-MAC317.local>
2026-03-19 08:26:33 -0300

    primiero commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+      $cidade = "Bauru";
+      $ano = 2026;
+      $mes = "Março";
+      $dia = 19;
+      echo "<b>$cidade</b>, $dia de $mes de $ano"
+     /*Esse programa exibe um cabeçalho com dia, mês e ano. Os dados mostrados no 
+     cabeçalho estão armazenados em suas devidas variaveis. */
+        
+?>
