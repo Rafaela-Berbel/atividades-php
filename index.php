30ad7dbdaa7ad7c1cae00804fd98ea3343b5cfe9
Rafaela-Berbel <rafaela.berbel@unesp.br>
2026-03-19 08:30:13 -0300

    primeiro commit

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-      $cidade = "Bauru";
       $ano = 2026;
       $mes = "Março";
-      $dia = 19;
+      $dia = 12;
+      $cidade = "Bauru";
       echo "<b>$cidade</b>, $dia de $mes de $ano"
      /*Esse programa exibe um cabeçalho com dia, mês e ano. Os dados mostrados no 
      cabeçalho estão armazenados em suas devidas variaveis. */
